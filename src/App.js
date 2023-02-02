a859b49109816ce8e48098cbd765a91ef9fe931b
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-02-02 10:28:55 +0530

    Added debounce with useRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import { debounce } from 'lodash';
 
-function App() {
+
+const getRepos = async (username) => {
+  console.log('called with', username)
+  const res = await fetch(`https://api.github.com/users/${username}/repos`);
+  return res.json();
+};
+
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [data, setData] = useState([]);
+
+  async function handler(username) {
+    const res = await getRepos(username);
+    if (Array.isArray(res)) setData(res);
+  }
+
+  const debouncedRef = useRef(debounce(handler, 2000))
+
+  useEffect(() => {
+    debouncedRef.current(username);
+  }, [username]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        value={username}
+        onChange={(event) => {
+          setUsername(event.target.value);
+          // console.log("called with", event.target.value);
+        }}
+        placeholder="Enter github username"
+        type="text"
+      />
+      <h2>Getting repos for {username}</h2>
+      <ol>
+        {data.map((el) => {
+          return <li key={el.name}>{el.name}</li>;
+        })}
+      </ol>
+    </>
   );
-}
+};
 
 export default App;
